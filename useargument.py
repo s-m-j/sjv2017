2ab0f4816a844965df8827b7f73a9e7465c2b6b8
Sergey Maslov <sergey.s.maslov@gmail.com>
2021-01-03 13:56:47 +0300

    Использование аргументов командной строки

--- useargument.py
+++ useargument.py
@@ -1,7 +1,7 @@
 import sys
 import stdio
 
-#Использование аргументов командной строки
+#Использование аргументов командной строки 
 stdio.write('Hi, ')
 stdio.write(sys.argv[1])
 stdio.writeln('. How are you?')
