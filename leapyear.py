2f130b793a752898b930d1a0820eefc3c655f49d
Sergey Maslov <sergey.s.maslov@gmail.com>
2021-01-03 14:15:35 +0300

    Високосный год (проверка)

--- leapyear.py
+++ leapyear.py
@@ -5,4 +5,6 @@ year = float(sys.argv[1])
 
 isLeapYear = (year % 4 == 0)
 isLeapYear = isLeapYear and ((year % 100 != 0))
-isLeapYear = isLeapYear or ((yaear % 400 == 0))
+isLeapYear = isLeapYear or ((year % 400 == 0))
+
+stdio.writeln(isLeapYear)
