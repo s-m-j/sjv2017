c392f37a0feed4d71180c365eb9239d9c4c74173
Sergey Maslov <sergey.s.maslov@gmail.com>
2021-01-03 14:01:22 +0300

    Операции с float

---
+++ floatops.py
@@ -0,0 +1,18 @@
+import sys
+import stdio
+
+a = float(sys.argv[1])
+b = flost(sys.argv[2])
+
+total = a + b
+diff = a - b
+prod = a * b
+quot = a / b
+exp = a ** b
+
+stdio.writeln(str(a) + ' + ' + str(b) + ' = ' str(total))
+stdio.writeln(str(a) + ' - ' + str(b) + ' = ' str(diff))
+stdio.writeln(str(a) + ' * ' + str(b) + ' = ' str(prod))
+stdio.writeln(str(a) + ' / ' + str(b) + ' = ' str(quot))
+stdio.writeln(str(a) + ' ** ' + str(b) + ' = ' str(exp))
+
